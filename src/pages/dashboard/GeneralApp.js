0add270287f90bf3432218f3674a8406430d10b2
Prathu Gupta <developer.prathugupta@gmail.com>
2023-03-13 01:03:15 +0530

    completed conversation & message section

--- src/pages/dashboard/GeneralApp.js
+++ src/pages/dashboard/GeneralApp.js
@@ -1,15 +1,26 @@
-import { Stack } from "@mui/material";
+import { Box, Stack } from "@mui/material";
 import React from "react";
+import Conversation from "../../components/Conversation";
 import Chats from "./Chats";
+import { useTheme } from "@mui/material/styles";
 
 const GeneralApp = () => {
-
+  const theme = useTheme();
   return (
-    <Stack direction={"row"} sx={{width: "100%"}}>
-    {/* Chats */}
-    <Chats />
+    <Stack direction={"row"} sx={{ width: "100%" }}>
+      {/* Chats */}
+      <Chats />
 
-    {/* Conversation */}
+      <Box
+        sx={{
+          height: "100%",
+          width: "calc(100vw - 420px)",
+          backgroundColor: theme.palette.mode === "light" ? "#fff" : theme.palette.background.default,
+        }}
+      >
+        {/* Conversation */}
+        <Conversation />
+      </Box>
     </Stack>
   );
 };
